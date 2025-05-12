3602d1f504e90f2fbcdd19bfb074919c34338a36
Anglade Loïc <jimmynicriket140@gmail.com>
2025-05-12 14:36:16 +0200

    CV-BOOTSTRAP-FLIP maj code, insertion fonction js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 function flipCard() {
   document.getElementById("cvFlip").classList.toggle("flipped");
 }
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    document.querySelectorAll(".progress-fill").forEach((bar) => {
+      const value = bar.getAttribute("data-value");
+      bar.style.width = value + "%";
+    });
+  }, 1500);
+});
